889a256f3a20f3c8a1e13c3003caf2efb8b350d4
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2024-04-29 16:06:34 -0300

    Agrego hook useSearch

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,51 @@
 
 import './App.css'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
-function App() {
-  const {movies} = useMovies()
-  const [query, setQuery] = useState("")
+function useSearch (){
+  const [search, setSearch] = useState("")
   const [error, setError] = useState(null)
-  // const inputRef = useRef()
-
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    // usando useRef
-    // const value = inputRef.current.value
-
-    // Obtener un input de un form a partir de su atributo name
-    // const fields = new window.FormData(event.target)
-    // const query = fields.get('query')
-
-    // Para leer todos los inputs de un Form    
-    // const {query} = Object.fromEntries(new window.FormData(event.target))
-    // setQuery(query)
-  }
-
-  const handleChange = (event) =>{
-    const newQuery = event.target.value
-    if (newQuery.startsWith(' ')) return
-    setQuery(newQuery)
-  }
 
   useEffect(() => {
-    if (query === '') {
+    if (search === '') {
       setError('Debe ingresar un texto para iniciar la búsqueda')      
       return
     }
 
-    if (query.match(/^\d+$/)){
+    if (search.match(/^\d+$/)){
       setError('Debe ingresar un contenido alfanumérico')
       return
     }
 
-    if (query.length <= 3){
+    if (search.length <= 3){
       setError('La búsuqeda debe tener al menos 3 caracteres')
       return
     }
 
     setError(null)
 
-  },[query])
+  },[search])
+
+  return {search, setSearch, error}
+}
+
+function App() {
+  const {movies} = useMovies()
+  const {search, setSearch, error} = useSearch()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+  }
+
+  const handleChange = (event) =>{
+    const newQuery = event.target.value
+    if (newQuery.startsWith(' ')) return
+    setSearch(newQuery)
+  }
+
+  
 
 
     return (
@@ -58,7 +55,7 @@ function App() {
       <form className="form" onSubmit={handleSubmit}>
         <input 
           onChange={handleChange}
-          value={query}
+          value={search}
           name='query'
           type="text"
           style={{border: '1px solid transparent', borderColor: error ? 'red' : 'green'}}
