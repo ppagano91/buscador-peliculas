5945adec9a7517719865e03d2a0005cedc984999
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2024-04-29 15:41:47 -0300

    usando useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 
 import './App.css'
+import { useRef } from 'react'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
-
-
-
 function App() {
-  const {movies} = useMovies() 
+  const {movies} = useMovies()
+  const inputRef = useRef()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const value = inputRef.current.value
+  }
+
+
 
     return (
     <div className='page'>
     <header>
       <h1 className=''>Buscador de Películas</h1>
-      <form className="form">
-        <input type="text" placeholder='Avengers, Star Wars, Rey León...' />
+      <form className="form" onSubmit={handleSubmit}>
+        <input name='query' ref={inputRef} type="text" placeholder='Avengers, Star Wars, Rey León...' />
         <button type="submit">Buscar</button>
       </form>
     </header>
